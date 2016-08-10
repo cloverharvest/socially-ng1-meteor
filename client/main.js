4dd52c0dcaf6e3a1e67dbd5a8078fda3f5e1228c
cloverharvest <anna@schwabdev.com>
2016-08-10 16:09:20 -0700

    Adds party list

--- client/main.js
+++ client/main.js
@@ -7,14 +7,9 @@ angular.module('socially-ng1', [
     .controller('PartiesListCtrl', function($scope) {
         'ngInject';
 
-        $scope.parties = [{
-            'name': 'Dubstep-Free Zone',
-            'description': 'Can we please just for an evening not listen to dubstep'
-        }, {
-            'name': 'All dubstep all the time',
-            'description': 'Get it again.'
-        }, {
-            'name': 'Savage lounging',
-            'description': 'Leisure suit required.'
-        }];
+        $scope.helpers({
+            parties() {
+                return Parties.find({});
+            }
+        })
     });
