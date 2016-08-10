baf5ada50bb635092f96e98786dcc829895efde8
cloverharvest <anna@schwabdev.com>
2016-08-10 14:55:18 -0700

    Adds controller and model

--- client/main.js
+++ client/main.js
@@ -3,5 +3,18 @@ import angularMeteor from 'angular-meteor';
 
 angular.module('socially-ng1', [
     angularMeteor
-]);
+  ])
+    .controller('PartiesListCtrl', function($scope) {
+        'ngInject';
 
+        $scope.parties = [{
+            'name': 'Dubstep-Free Zone',
+            'description': 'Can we please just for an evening not listen to dubstep'
+        }, {
+            'name': 'All dubstep all the time',
+            'description': 'Get it again.'
+        }, {
+            'name': 'Savage lounging',
+            'description': 'Leisure suit required.'
+        }];
+    });
