8c90ade81e4f09c31448685f70874ffbaf152399
cloverharvest <anna@schwabdev.com>
2016-08-15 20:09:19 -0700

    Install ui-router, add base tag, add uiView directive, set html5 and parties as root

--- imports/ui/components/socially/socially.js
+++ imports/ui/components/socially/socially.js
@@ -1,5 +1,6 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
+import uiRouter from 'angular-ui-router';
 
 import template from './socially.html';
 import { name as PartiesList } from '../partiesList/partiesList';
@@ -11,9 +12,19 @@ const name = 'socially';
 //create a module
 export default angular.module(name, [
     angularMeteor,
+    uiRouter,
     PartiesList
 ]).component(name, {
     template,
     controllerAs: name,
     controller: Socially
-});
+})
+    .config(config);
+
+function config($locationProvider, $urlRouterProvider) {
+    'ngInject';
+
+    $locationProvider.html5Model(true);
+
+    $urlRouterProvider.otherwise('/parties');
+}
